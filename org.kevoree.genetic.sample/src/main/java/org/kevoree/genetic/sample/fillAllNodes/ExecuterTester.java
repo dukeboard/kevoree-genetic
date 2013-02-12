f9104eb681a113e05ef996b3c06764d505e84c71
Johann Bourcier <jbourcie@irisa.fr>
2013-02-12 17:09:33 +0100

    executer test

---
+++ org.kevoree.genetic.sample/src/main/java/org/kevoree/genetic/sample/fillAllNodes/ExecuterTester.java
@@ -0,0 +1,55 @@
+package org.kevoree.genetic.sample.fillAllNodes;
+
+import org.kevoree.genetic.framework.KevoreeFitnessFunction;
+import org.kevoree.genetic.framework.internal.KevoreeProblem;
+import org.kevoree.genetic.framework.internal.KevoreeVariationAdaptor;
+import org.moeaframework.algorithm.Checkpoints;
+import org.moeaframework.algorithm.NSGAII;
+import org.moeaframework.core.*;
+import org.moeaframework.core.operator.TournamentSelection;
+import org.moeaframework.core.spi.AlgorithmFactory;
+import org.moeaframework.util.distributed.DistributedProblem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jbourcie
+ * Date: 12/02/13
+ * Time: 16:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExecuterTester {
+
+    public static void main(String[] args) {
+        Problem problem = null;
+        List<KevoreeFitnessFunction> fitnesses = new ArrayList<KevoreeFitnessFunction>();
+        fitnesses.add(new FillAllFitness());
+        Variation variation = new KevoreeVariationAdaptor(new AddComponentMutator("FakeConsole"));
+        Algorithm algorithm = new NSGAII(new KevoreeProblem(fitnesses), new NondominatedSortingPopulation(), new EpsilonBoxDominanceArchive(0.5), new TournamentSelection(),variation ,new Initialization() );
+        int maxEvaluations = 10000;
+        try {
+
+//               if (instrumenter != null) {
+//                    algorithm = instrumenter.instrument(algorithm);
+//                }
+
+            while (!algorithm.isTerminated() &&
+                    (algorithm.getNumberOfEvaluations() < maxEvaluations)) {
+                algorithm.step();
+            }
+
+            NondominatedPopulation result = new NondominatedPopulation();
+            result.addAll(algorithm.getResult());
+        } finally {
+            if (algorithm != null) {
+                algorithm.terminate();
+            }
+            if (problem != null) {
+                problem.close();
+            }
+
+        }
+    }
+}
