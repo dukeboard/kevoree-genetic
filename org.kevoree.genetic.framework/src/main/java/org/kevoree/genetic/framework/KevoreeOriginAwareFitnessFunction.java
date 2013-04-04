440bf366eeb108166572cb3fc27c9f38dea430e9
Francois Fouquet <fouquet.f@gmail.com>
2013-04-04 23:42:54 +0200

    cleanup

---
+++ org.kevoree.genetic.framework/src/main/java/org/kevoree/genetic/framework/KevoreeOriginAwareFitnessFunction.java
@@ -0,0 +1,13 @@
+package org.kevoree.genetic.framework;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: duke
+ * Date: 27/03/13
+ * Time: 14:42
+ */
+public abstract class KevoreeOriginAwareFitnessFunction implements KevoreeFitnessFunction {
+
+
+
+}
