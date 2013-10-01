0da8136c64cf24eeec20e87ca634659e582df698
doniaelkateb <donia.elkateb@uni.lu>
2013-10-01 16:37:06 +0200

    execution model

---
+++ org.kevoree.modeling.optimization.framework.execution.model/src/main/java/org/kevoreeoptimization/GeneratedAspect_Hypervolume.kt
@@ -0,0 +1,7 @@
+package org.kevoreeoptimization;
+import org.kevoree.modeling.api.aspect;
+public aspect trait GeneratedAspect_Hypervolume : org.kevoreeoptimization.Hypervolume {
+	override fun update(_:org.kevoreeoptimization.Score){
+		throw Exception("Not implemented yet !");
+	}
+}
