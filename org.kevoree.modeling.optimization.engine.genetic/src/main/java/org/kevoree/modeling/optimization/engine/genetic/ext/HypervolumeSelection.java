cff5e260cbde9b33050c7c3dfe8570b0d72f34b3
doniaelkateb <donia.elkateb@uni.lu>
2013-10-09 12:50:36 +0200

    hypervolume selection fixed

--- org.kevoree.modeling.optimization.engine.genetic/src/main/java/org/kevoree/modeling/optimization/engine/genetic/ext/HypervolumeSelection.java
+++ org.kevoree.modeling.optimization.engine.genetic/src/main/java/org/kevoree/modeling/optimization/engine/genetic/ext/HypervolumeSelection.java
@@ -1,37 +1,57 @@
 
 package org.kevoree.modeling.optimization.engine.genetic.ext;
+
 import org.moeaframework.core.Population;
 import org.moeaframework.core.Selection;
 import org.moeaframework.core.Solution;
 
-public class HypervolumeSelection implements Selection {
+
+public class HypervolumeSelection implements Selection  {
 
     private final HypervolumeComparator comparator;
 
+
+
     public HypervolumeSelection(HypervolumeComparator comparator) {
         this.comparator = comparator;
+
     }
 
     @Override
-    public Solution[] select(int arity, Population population) {
-        Solution[] solution = new Solution[arity];
-        //determine composition of next population
+    public Solution[]  select(int arity, Population population) {
+
         //determine composition of next population
         Population offspring = new Population();
+        Solution[] solution = new Solution[arity];
 
         for (int i = 0; i < population.size(); i++) {
-            int result = comparator.compare(offspring.get(i), population.get(i));
+            int result = comparator.compare(population.get(i), population.get(population.size()-i));
 
-            if (result < 0) {
-                offspring.add(offspring.get(i));
-            } else if (result > 0) {
-                offspring.add(population.get(i));
-            } else {
-                offspring.add(offspring.get(i));
-                offspring.add(population.get(i));
+            while (offspring.size() < arity) {
+
+                if (result < 0) {
+                    offspring.add(population.get(i));
+                } else if (result > 0) {
+                    offspring.add(population.get(population.size()-i));
+                } else {
+                    offspring.add(population.get(i));
+                    offspring.add(population.get(population.size()-i));
+                }
+
+
             }
+
+
+
         }
-        return solution;
+
+        for (int i = 0; i < arity; i++) {
+            solution[i] = population.get(i);
+        }
+
+     return  solution;
+
+
     }
 
 }
