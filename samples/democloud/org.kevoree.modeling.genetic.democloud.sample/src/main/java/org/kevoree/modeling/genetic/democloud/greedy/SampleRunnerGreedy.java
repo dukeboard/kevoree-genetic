fe8b0469742e5329a3deab6cbe8bc3789eec9a77
doniaelkateb <donia.elkateb@uni.lu>
2013-10-16 00:05:07 +0200

    greedy test

---
+++ samples/democloud/org.kevoree.modeling.genetic.democloud.sample/src/main/java/org/kevoree/modeling/genetic/democloud/greedy/SampleRunnerGreedy.java
@@ -0,0 +1,93 @@
+package org.kevoree.modeling.genetic.democloud.greedy;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: donia.elkateb
+ * Date: 10/15/13
+ * Time: 11:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import org.cloud.Cloud;
+import java.util.List;
+import java.io.File;
+import org.kevoree.modeling.genetic.democloud.CloudPopulationFactory;
+import org.kevoree.modeling.genetic.democloud.fitnesses.CloudCostFitness;
+import org.kevoree.modeling.genetic.democloud.fitnesses.CloudLatencyFitness;
+import org.kevoree.modeling.genetic.democloud.fitnesses.CloudRedundancyFitness;
+import org.kevoree.modeling.genetic.democloud.fitnesses.CloudSimilarityFitness;
+
+import org.kevoree.modeling.genetic.democloud.mutators.SmartMutator.AddSmartMutator;
+import org.kevoree.modeling.genetic.democloud.mutators.SmartMutator.RemoveSmartMutator;
+import org.kevoree.modeling.optimization.api.ParetoFitnessMetrics;
+import org.kevoree.modeling.optimization.api.ParetoMetrics;
+import org.kevoree.modeling.optimization.engine.greedy.GreedyEngine;
+import org.kevoree.modeling.optimization.api.Solution;
+import org.kevoree.modeling.optimization.executionmodel.ExecutionModel;
+import org.kevoree.modeling.optimization.framework.SolutionPrinter;
+import org.kevoree.modeling.optimization.util.ExecutionModelExporter;
+
+import org.kevoree.modeling.genetic.democloud.mutators.AddNodeMutator;
+import org.kevoree.modeling.genetic.democloud.mutators.RemoveNodeMutator;
+import org.kevoree.modeling.genetic.democloud.mutators.RemoveSoftwareMutator;
+import org.kevoree.modeling.genetic.democloud.mutators.CloneNodeMutator;
+
+import org.kevoree.modeling.genetic.democloud.mutators.AddSoftwareMutator;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: donia.elkateb
+ * Date: 10/10/13
+ * Time: 9:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SampleRunnerGreedy {
+
+
+
+    public static void main(String[] args) throws Exception {
+
+        GreedyEngine<Cloud> engine = new GreedyEngine<Cloud>();
+
+        //engine.desactivateOriginAware();
+
+
+
+        engine.addOperator(new AddNodeMutator());
+        engine.addOperator(new RemoveNodeMutator());
+        engine.addOperator(new AddSoftwareMutator());
+
+        engine.addOperator(new CloneNodeMutator());
+
+        engine.addOperator(new RemoveSoftwareMutator());
+        engine.addOperator(new AddSmartMutator());
+        engine.addOperator(new RemoveSmartMutator());
+
+        engine.addFitnessFuntion(new CloudCostFitness());
+        engine.addFitnessFuntion(new CloudLatencyFitness());
+        engine.addFitnessFuntion(new CloudRedundancyFitness());
+        engine.addFitnessFuntion(new CloudSimilarityFitness());
+        //engine.addFitnessFuntion(new CloudAdaptationFitness());
+
+
+        engine.setMaxGeneration(50);
+        engine.setPopulationFactory(new CloudPopulationFactory().setSize(10));
+
+
+        engine.addFitnessMetric(new CloudCostFitness(), ParetoFitnessMetrics.Min);
+        engine.addFitnessMetric(new CloudCostFitness(), ParetoFitnessMetrics.Max);
+        engine.addParetoMetric(ParetoMetrics.Mean);
+
+        List<Solution<Cloud>> result = engine.solve();
+
+        SolutionPrinter printer = new SolutionPrinter();
+        for (Solution sol : result) {
+            printer.print(sol, System.out);
+        }
+
+        ExecutionModel model = engine.getExecutionModel();
+        ExecutionModelExporter.instance$.exportMetrics(model,new File("results"));
+
+    }
+}
