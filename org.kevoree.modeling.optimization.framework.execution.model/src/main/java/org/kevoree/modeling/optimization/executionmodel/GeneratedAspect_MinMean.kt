264adfebcf667ba3b465dd15ffad369e4a4e3f13
doniaelkateb <donia.elkateb@uni.lu>
2013-10-29 17:18:48 +0100

    MinMean

--- org.kevoree.modeling.optimization.framework.execution.model/src/main/java/org/kevoree/modeling/optimization/executionmodel/GeneratedAspect_MinMean.kt
+++ org.kevoree.modeling.optimization.framework.execution.model/src/main/java/org/kevoree/modeling/optimization/executionmodel/GeneratedAspect_MinMean.kt
@@ -2,9 +2,9 @@ package org.kevoree.modeling.optimization.executionmodel;
 import org.kevoree.modeling.api.aspect;
 public aspect trait GeneratedAspect_MinMean : org.kevoree.modeling.optimization.executionmodel.MinMean {
     override fun update() {
-        if(value == null){
+
             value = 10000000.toDouble()
-        }
+
         var currentStep: Step = eContainer() as Step
         for(solution in currentStep.solutions){
             var nb = 0
