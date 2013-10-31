45eb780730d310d7c9fc1c79a3e921d2efe29fff
Francois Fouquet <fouquet.f@gmail.com>
2013-10-31 10:03:10 +0100

    cleanup

--- org.kevoree.modeling.optimization.framework.core/src/main/java/org/kevoree/modeling/optimization/framework/RulesFitnessFunction.kt
+++ org.kevoree.modeling.optimization.framework.core/src/main/java/org/kevoree/modeling/optimization/framework/RulesFitnessFunction.kt
@@ -6,6 +6,7 @@ import org.kevoree.modeling.optimization.api.Rule
 import java.util.ArrayList
 import org.kevoree.modeling.optimization.api.fitness.FitnessFunction
 import org.kevoree.modeling.optimization.api.GenerationContext
+import org.kevoree.modeling.optimization.api.fitness.FitnessOrientation
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,6 +17,16 @@ import org.kevoree.modeling.optimization.api.GenerationContext
 
 public class RulesFitnessFunction<A : KMFContainer> : FitnessFunction<A> {
 
+    override fun max(): Double {
+        return rules.size.toDouble()
+    }
+    override fun min(): Double {
+        return 0.0;
+    }
+    override fun orientation(): FitnessOrientation {
+       return FitnessOrientation.MINIMIZE
+    }
+
     var rules: MutableList<Rule<A>> = ArrayList<Rule<A>>()
 
     public fun addRule(rule: Rule<A>): RulesFitnessFunction<A> {
@@ -24,14 +35,15 @@ public class RulesFitnessFunction<A : KMFContainer> : FitnessFunction<A> {
     }
 
     override fun evaluate(model: A, context : GenerationContext<A>): Double {
-        var nbViolatedRules = 0
+        var nbViolatedRules = 0.0
         for(rule in rules){
             if(!rule.check(model)){
                 nbViolatedRules++
             }
         }
-        val pres = ( (rules.size() - nbViolatedRules) / rules.size()) * 100;
-        return 100 - Math.min(Math.max(0.toDouble(), pres.toDouble()), 100.toDouble())
+        return nbViolatedRules;
+        //val pres = ( (rules.size() - nbViolatedRules) / rules.size()) * 100;
+        //return 100 - Math.min(Math.max(0.toDouble(), pres.toDouble()), 100.toDouble())
     }
 
 }
