67022dbc98cc12dc26050516c9a8d734ba8a3a7b
Francois Fouquet <fouquet.f@gmail.com>
2013-10-31 13:47:28 +0100

    add nprmalizer

--- org.kevoree.modeling.optimization.framework.core/src/main/java/org/kevoree/modeling/optimization/util/FitnessNormalizer.kt
+++ org.kevoree.modeling.optimization.framework.core/src/main/java/org/kevoree/modeling/optimization/util/FitnessNormalizer.kt
@@ -1,9 +1,27 @@
 package org.kevoree.modeling.optimization.util
 
+import org.kevoree.modeling.optimization.api.fitness.FitnessFunction
+import org.kevoree.modeling.optimization.api.fitness.FitnessOrientation
+
 /**
  * Created with IntelliJ IDEA.
  * User: duke
  * Date: 31/10/2013
  * Time: 10:26
- * To change this template use File | Settings | File Templates.
  */
+
+public object FitnessNormalizer {
+
+    /* Transform raw value between 0 and 1 and 0 is the best value */
+    fun norm(rawValue: Double, fitness: FitnessFunction<*>): Double {
+        val minValue = fitness.min()
+        val maxValue = fitness.max()
+        val normalizedValue = (rawValue - minValue) / (maxValue - minValue)
+        if(fitness.orientation() == FitnessOrientation.MAXIMIZE){
+            return maxValue - normalizedValue
+        } else {
+            return normalizedValue
+        }
+    }
+
+}
