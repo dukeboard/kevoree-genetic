0c96eb5802c9a676e032110f8ce2c508f60dca38
Francois Fouquet <fouquet.f@gmail.com>
2013-10-31 10:20:44 +0100

    cleanup composite :-)

--- org.kevoree.modeling.optimization.framework.core/src/main/java/org/kevoree/modeling/optimization/framework/CompositeFitnessFunction.kt
+++ org.kevoree.modeling.optimization.framework.core/src/main/java/org/kevoree/modeling/optimization/framework/CompositeFitnessFunction.kt
@@ -4,6 +4,7 @@ import org.kevoree.modeling.api.KMFContainer
 import java.util.ArrayList
 import org.kevoree.modeling.optimization.api.fitness.FitnessFunction
 import org.kevoree.modeling.optimization.api.GenerationContext
+import org.kevoree.modeling.optimization.api.fitness.FitnessOrientation
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,6 +13,22 @@ import org.kevoree.modeling.optimization.api.GenerationContext
  * Time: 16:20
  */
 public class CompositeFitnessFunction<A : KMFContainer> : FitnessFunction<A> {
+
+    var minValue: Double = java.lang.Double.MAX_VALUE
+
+    var maxValue: Double = java.lang.Double.MIN_VALUE
+
+    var orientationValue: FitnessOrientation = FitnessOrientation.MINIMIZE
+
+    override fun max(): Double {
+        return maxValue
+    }
+    override fun min(): Double {
+        return minValue
+    }
+    override fun orientation(): FitnessOrientation {
+        return orientationValue
+    }
 
     override fun evaluate(model: A, context: GenerationContext<A>): Double {
         var value = 0.0;
@@ -24,6 +41,22 @@ public class CompositeFitnessFunction<A : KMFContainer> : FitnessFunction<A> {
     val fitnesses = ArrayList<FitnessFunction<A>>();
 
     public fun addFitness(newfit: FitnessFunction<A>): CompositeFitnessFunction<A> {
+        if(fitnesses.isEmpty()){
+            orientationValue = newfit.orientation()
+        } else {
+            if(newfit.orientation() != orientationValue){
+                System.err.println("Unconsistancy in composite fitness all fitness must have the same orientation")
+                throw Exception("Unconsistancy composite fitness errror")
+            }
+        }
+        val currentFitMin = newfit.min();
+        if(currentFitMin < minValue){
+            minValue = currentFitMin
+        }
+        val currentFitMax = newfit.max();
+        if(currentFitMax > maxValue){
+            maxValue = currentFitMax
+        }
         fitnesses.add(newfit);
         return this;
     }
