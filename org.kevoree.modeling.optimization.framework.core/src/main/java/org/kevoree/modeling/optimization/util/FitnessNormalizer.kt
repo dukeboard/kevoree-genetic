20de738ca0d5c1ef4ec3052b88fdcded256d5bf6
Francois Fouquet <fouquet.f@gmail.com>
2013-10-31 11:08:15 +0100

    add normalizer

---
+++ org.kevoree.modeling.optimization.framework.core/src/main/java/org/kevoree/modeling/optimization/util/FitnessNormalizer.kt
@@ -0,0 +1,9 @@
+package org.kevoree.modeling.optimization.util
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: duke
+ * Date: 31/10/2013
+ * Time: 10:26
+ * To change this template use File | Settings | File Templates.
+ */
